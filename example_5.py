a7c194ae39f3129d79afcefbdb3f21a315e274e1
Oleg <amstl@mail.ru>
2023-10-28 18:02:38 +0700

    Function shield

--- example_5.py
+++ example_5.py
@@ -1,11 +1,12 @@
 import asyncio
 from util import delay
 
+
 async def main():
     task = asyncio.create_task(delay(10))
 
     try:
-        result = await asyncio.wait_for(task, timeout=5)
+        result = await asyncio.wait_for(asyncio.shield(task), 5)
         print(result)
     except TimeoutError:
         print("Задача заняла более 5 секунд, скоро она закончится!")
